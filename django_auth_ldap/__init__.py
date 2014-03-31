5dc523603dea2f5ae88c1e617004b8119c240eef
Peter Sagerson <psagers.hg@ignorare.net>
2014-03-31 13:00:53 -0700

    Fix cidict test.
    Need to drop Django 1.3 support to get django.utils.six. Also some PEP-8
    cleanup.

---
+++ django_auth_ldap/__init__.py
@@ -0,0 +1,2 @@
+version = (1, 1, 8)
+version_string = '.'.join(map(str, version))
---
+++ test/settings.py
@@ -0,0 +1,29 @@
+# For older versions of Djano
+DATABASE_ENGINE = 'sqlite3'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ALLOWED_HOSTS = []
+
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = False
+USE_L10N = False
+USE_TZ = True
+
+SECRET_KEY = 'nt56v8)moa)37ta5z7dd=if-@y#k@l7+t8lct*c8m730lpd=so'
+
+ROOT_URLCONF = 'urls'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+
+    'django_auth_ldap',
+)
